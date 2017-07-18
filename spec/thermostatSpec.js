d8202fe77f5d5321a0be065a097df2b2488a2b79
glynester <glynester@hotmail.com>
2017-07-18 15:38:11 +0100

    Test and func for min temp

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -18,5 +18,14 @@ beforeEach(function() {
   it("should decrease the temp by 1 when the down function is run", function (){
     thermostat.turnDown();
     expect(thermostat.temp).toEqual(19);
+  });
+
+  it("should have a minimum temp of 10c", function(){
+    for (var i=1;i<=15; i++){
+      thermostat.turnDown();
+    }
+    expect(thermostat.temp).toEqual(10);
+  });
+
 
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,4 +1,5 @@
 function Thermostat() {
+  var MINTEMP = 10;
 }
 Thermostat.prototype.turnOn = function() {
   this.temp = 20;
@@ -8,6 +9,8 @@ Thermostat.prototype.turnUp = function() {
   this.temp += 1;
 }
 
-Thermostat.protype.turnDown = function() {
-  this.temp -= 1;
+Thermostat.prototype.turnDown = function() {
+  if (this.temp>10){
+    this.temp -= 1;
+  }
 }
